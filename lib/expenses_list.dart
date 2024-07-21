858f5655d5f1e657108b0e66b478d45fb0c451a3
Marcello Bozzi <bozzimarcello@gmail.com>
2024-07-21 17:04:57 +0200

    expenses list widget setup

---
+++ lib/expenses_list.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'package:expense_tracker/models/expense.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+  });
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    // ListView.builder create the items only if they are visible
+    // to be used when there are many items
+    // instead of ListView, which creates all items at once
+    // or Column, which creates all items at once and doesn't have scrolling
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (ctx, index) => Text(expenses[index].title),
+    );
+  }
+}
