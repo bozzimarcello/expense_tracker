91dc6d42cfe9ab1c6e31558b162840dae9fd3143
Marcello Bozzi <bozzimarcello@gmail.com>
2024-07-23 15:55:20 +0200

    general theme data created

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
-var kColorScheme = ColorScheme.fromSeed( 
+var kColorScheme = ColorScheme.fromSeed(
   // ColorScheme.fromSeed is a constructor that creates a ColorScheme from a seed color
   // so you can create a color scheme with a primary color and the others are generated from it
   seedColor: const Color.fromARGB(255, 176, 46, 228),
@@ -11,13 +11,32 @@ var kColorScheme = ColorScheme.fromSeed(
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData().copyWith( 
+      theme: ThemeData().copyWith(
         // copyWith lets you create a new ThemeData changing only the properties you want
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
           backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primary,
-        )
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.normal,
+            color: kColorScheme.onSecondaryContainer,
+          ),
+        ),
       ),
       home: const Expenses(),
     ),
